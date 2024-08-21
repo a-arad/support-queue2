a23d4bc92f151d2c1b4fccaf33fafe2a2f224849
a-arad <arad.coding@gmail.com>
2024-08-21 14:44:31 -0400

    added a query

--- connect_and_query.py
+++ connect_and_query.py
@@ -1,9 +1,17 @@
 import os
 from dotenv import load_dotenv
 import psycopg2 
+import pandas as pd
 
 load_dotenv()
 
 conn = psycopg2.connect(os.getenv("DB_CONNECTION_STRING"))
-print(conn.status)
+
+query = "select * from companies limit 5;"
+
+data = pd.read_sql(query,conn)
+
+print(data.head())
+
+
 
